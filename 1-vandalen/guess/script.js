8614fb172b07cf3f70166f46b4a66cd479a08d41
cg222im <cg222im@student.lnu.se>
2014-11-13 17:04:33 +0000

    L01 1.1 completed.

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,38 +2,39 @@
 
 window.onload = function(){
 	
-	// Slumpar fram tal mellan 1-100
-	var secret = Math.floor( (Math.random() * 100) +1);
+	// Slumpar fram tal mellan 0-100
+	var secret = Math.floor( Math.random() * 101);
 	
-	// Hanterar antal gissningar
-	var guessCount = 0; 
-	
-	
-	// I denna funktion ska du skriva koden för att hantera "spelet"
+	var guessCount = 0;
 	
 	var guess = function(number){
 		
-		
 		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
 		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
-			
-		// Plats för förändring.
-		if (number === secret){
-			console.Log("Grattis du vann! Det hemliga talet var "+secret+" och du behövde "+guessCount+" gissningar för att hitta det.");
-			return true;
-		};
-		if (number > secret){
-			
-		};
-		if (number < secret){
-			
-		};
-
-		// Returnera exempelvis: 
-		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
-		// [false, "Det hemliga talet är högre!"]
-		// [false, "Det hemliga talet är lägre!"]
-		// [false, "Talet är utanför intervallet 0 - 100"]		
+		
+		// Felhantering av icke-numeriska värden
+		if (isNaN(number) === true || number === ""){
+			return [false, "Endast siffror, tack!"];
+		}
+		// Felhantering av för stora eller små värden
+		else if (number < 0 || number > 100){
+			return [false, "Talet är utanför intervallet 0 - 100"];
+		}
+		
+		else if (number == secret){
+			guessCount += 1;
+			return [true, "Grattis du vann! Det hemliga talet var "+secret+" och du behövde "+guessCount+" gissningar för att hitta det."];
+		}
+		
+		else if (number > secret){
+			guessCount += 1;
+			return [false, "Det hemliga talet är lägre!"];
+		}
+		else if (number < secret){
+			guessCount += 1;
+			return [false, "Det hemliga talet är högre!"];
+		}
+	
 	};
 	
 	// ------------------------------------------------------------------------------
@@ -49,7 +50,7 @@ window.onload = function(){
 	submit.addEventListener("click", function(e){
 		e.preventDefault(); // Hindra formuläret från att skickas till servern. Vi hanterar allt på klienten.
 
-		var answer = guess(input.value) // Läser in talet från textrutan och skickar till funktionen "guess"
+		var answer = guess(input.value); // Läser in talet från textrutan och skickar till funktionen "guess"
 		p.innerHTML = answer[1];		// Skriver ut texten från arrayen som skapats i funktionen.	
 
 		if(answer[0] === true){				// Om spelet är slut, avaktivera knappen.
