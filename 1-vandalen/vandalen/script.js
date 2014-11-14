c996a8f9fe6e18cd263e241ac2ff82705ff2eeb0
cg222im <cg222im@student.lnu.se>
2014-11-14 14:41:54 +0000

    L01 4 function completed.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,8 +2,26 @@
 
 var makePerson = function(persArr){
 
+    persArr = ["John Häggerud", 37, "Johan Leitet", 36, "Mats Loock", 46];
+    
+    // Array med endast ålder        
+    var ageArr = [persArr[1], persArr[3], persArr[5]];
 
-	// Din kod här...
+    // Uträkningar med ålder
+    var minAge = Math.min.apply(Math, ageArr);
+    var maxAge = Math.max.apply(Math, ageArr);  
+    var averageAge = Math.round(((ageArr[0] + ageArr[1] + ageArr[2])/3));   
+   
+    // Läser in namn till ny sträng
+    var names = [persArr[0], persArr[2], persArr[4]];
+    // Sorterar sträng med namn
+    names.sort();
+    // Formaterar sträng med namn
+    names = names[0] + ", " + names[1] + ", " + names[2];
+    // Skapar slutgiltig sträng för presentation
+    var returnString = "Lägsta ålder: " + minAge + "\n" + "Högsta ålder: " + maxAge + "\n" + "Medelålder: " + averageAge + "\n" + names;
+    return returnString;
+
 
     /*
     Skapa pure function som tar array[n: namn, ålder] som argument och returnerar nytt objekt
