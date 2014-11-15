11ef98b549509d68693623eb61228aad814a3fa9
cg222im <cg222im@student.lnu.se>
2014-11-15 19:27:51 +0000

    L01 4 - back to square 3..

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,58 +2,59 @@
 
 var makePerson = function(persArr){
 
-    persArr = ["John Häggerud", 37, "Johan Leitet", 36, "Mats Loock", 46];
+    //persArr = ["John Häggerud", 37, "Johan Leitet", 36, "Mats Loock", 46];
+  
+    
+    
+    //var indata = [{name: "John Häggerud", born: "1976-02-23"}, {name: "Johan Leitet", born: "1977-12-12"},  {name: "Mats Loock", born: "1967-06-02"}];
     
     // Array med endast ålder        
-    var ageArr = [persArr[1], persArr[3], persArr[5]];
+    // var ageArr = [persArr[1], persArr[3], persArr[5]];
+    /*console.log(persArr[0]);
+    console.log(persArr[1]);
+    console.log(persArr[2]); */
+    var ageArr = persArr.map(function(ageArr){
+        ageArr = [persArr[0] + persArr[1] + persArr[2]];
+        ageArr = ageArr[1] + ageArr[3] + ageArr[5];
+    console.log(ageArr);
+    });
 
     // Uträkningar med ålder
     var minAge = Math.min.apply(Math, ageArr);
     var maxAge = Math.max.apply(Math, ageArr);  
     var averageAge = Math.round(((ageArr[0] + ageArr[1] + ageArr[2])/3));   
    
-    // Läser in namn till ny sträng
-    var names = [persArr[0], persArr[2], persArr[4]];
-    // Sorterar sträng med namn
-    names.sort();
+    // Läser in namn till array
+    
+    
+    
+    
+    
+    var names = persArr.map(function(names){
+        names = [persArr[0] + persArr[1] + persArr[2]];
+        names = names[0] + names[2] + names[4];
+        
+    });
+    
+    // Sorterar array med namn <--- ÅÄÖ??
+    names.sort(function(a,b) {
+        return a.localeCompare(b);
+    });
+    
     // Formaterar sträng med namn
-    names = names[0] + ", " + names[1] + ", " + names[2];
-    // Skapar slutgiltig sträng för presentation
-    var returnString = "Lägsta ålder: " + minAge + "\n" + "Högsta ålder: " + maxAge + "\n" + "Medelålder: " + averageAge + "\n" + names;
-    return returnString;
+    names = names.join(", ");
 
+    // Sätter result objekt och returnerar                                                       
+    var result = {averageAge: averageAge,
+	     maxAge: maxAge,
+	     minAge: minAge,
+	     names: names};
+
+    return result;
+
+ 
 
     /*
-    Skapa pure function som tar array[n: namn, ålder] som argument och returnerar nytt objekt
-    Returnerat objekt ska innehålla: högsta åldern, lägsta åldern, medelåldern samt sträng med sorterade namn
-    
-    name = sträng: för- och efternamn
-    age = nummer för ålder
-    
-    ex var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
-    
-    
-    Returnerat objekt:
-    minAge
-    maxAge
-    averageAge
-    names (Kalle Anka, Sixten Blå)
-    
-    
-    Testa genom:
-    --------------------------------------------------------
-    var makePerson = function(persArr) { var result = {};
-
-    --Lös uppgiften --
-
-    return result; } var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
-
-    var result = makePerson(data);
-
-    console.log(result);
-    
-    
-    
     * = utan loopar (for, while, do...while), if, switch
         felhantering: saker innehåller det de ska (här fås if/switch osv användas)
     */
