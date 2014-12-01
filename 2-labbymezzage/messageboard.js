8dee8f9be2aedf357d4d803dbc393f42ebfb7878
cg222im <cg222im@student.lnu.se>
2014-12-01 13:41:56 +0000

    l02 mom3 messages array up.

--- 2-labbymezzage/messageboard.js
+++ 2-labbymezzage/messageboard.js
@@ -1,9 +1,25 @@
 "use strict";
 
 
-function MessageBoard()
+function MessageBoard() //kan slänga in boardId för flera brädor
 {
+    // Skapar array för meddelanden
+    var messages = [];
     
+    this.getMessages = function()
+    {
+        return messages;
+    };
+    
+    this.addMessage = function(message)
+    {
+        messages.push(message);
+    };
+    
+    this.removeMessage = function (messageId)
+    {
+        messages.splice(messageId, 1);
+    };
 }
 
 window.onload = function(){
@@ -11,11 +27,13 @@ window.onload = function(){
     new MessageBoard("board1");
     
     //tester moment 2
+    /*
     var mess = new Message("Testmeddelande", new Date());
     alert(mess);
     alert(mess.getText());
     mess.setText("blesk");
     alert(mess);
+    */
 };
 
 
