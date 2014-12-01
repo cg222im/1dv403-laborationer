980e5df97a1302a0f7d3280f5e55e1b4457a0968
cg222im <cg222im@student.lnu.se>
2014-12-01 15:08:14 +0000

    l02 renderMessageBoard should be done; css-styles marked up

--- 2-labbymezzage/messageboard.js
+++ 2-labbymezzage/messageboard.js
@@ -1,7 +1,7 @@
 "use strict";
 
 
-function MessageBoard() //kan slänga in boardId för flera brädor
+function MessageBoard(boardId) //kan slänga in boardId för flera brädor
 {
     // Skapar array för meddelanden
     var messages = [];
@@ -20,6 +20,18 @@ function MessageBoard() //kan slänga in boardId för flera brädor
     {
         messages.splice(messageId, 1);
     };
+    
+    
+    var main = document.getELementById("main");
+    
+    // Laddar MessageBoard
+    this.renderMessageBoard(main, boardId);
+    
+    // Laddar antal meddelanden
+    this.updateNumberofMessages();
+    
+    // Laddar meddelanden
+    this.renderMessages();
 }
 
 MessageBoard.prototype.renderMessageBoard = function(main, _boardId)
@@ -31,16 +43,39 @@ MessageBoard.prototype.renderMessageBoard = function(main, _boardId)
     boardNode.id = _boardId;
     boardNode.className = "messageBoard";
     
+    // Skapar behållare för meddelanden
     var messageContainerNode = document.createElement("div");
     messageContainerNode.className="messageContainer";
     
+    // Skapar element som räknar antal meddelanden
     var numberOfMessages = document.createElement("div");
     numberOfMessages.className = "numberOfMessages";
     
+    // Skapar plats för input
     var textArea = document.createElement("textArea");
     textArea.className = "textAreaInput";
+    textArea.keypress = function(e)
+    {
+        that.textAreaEventHandler();
+        return false;
+    };
     
+    // Skapar knapp för att sända meddelande
+    var submitButton = document.createElement("button");
+    submitButton.className = "messageButton";
+    submitButton.innerHTML = "Many Message";
+    submitButton.onclick = function(e)
+    {
+        that.submitButtonOnClick(textArea); 
+        return false;
+    };
     
+    // Lägger till noder till DOMen
+    main.appendChild(boardNode);
+    boardNode.appendChild(messageContainerNode);
+    boardNode.appendChild(numberOfMessages);
+    boardNode.appendChild(textArea);
+    boardNode.appendChild(submitButton);
 }
 
 window.onload = function(){
